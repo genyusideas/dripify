6eb537d460dd1bd31f606e863be1edd0593627f1
Jeff Mataya <jeff.mataya@gmail.com>
2013-05-14 21:16:43 -0400

    Initial setup of the Rails app.
    Simple scaffold of the Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dripify::Application.config.secret_token = 'ed645c457188baaa84db4d7518099053e31ddb89a3f774c7c21e732f7f975f1574acb48aa765ff37ca5acfcf18f3ffcdff3bf7d01b9a86e163969bfc4558491d'
