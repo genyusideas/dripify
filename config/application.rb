e8152f7426680a8c4afdaa89423875d6b143c343
Jeff Mataya <jeff.mataya@gmail.com>
2013-05-18 00:45:04 -0400

    Set assets to not need initialization on precompile for Heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Dripify
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Setting needed for proper Heroku deployment.
+    config.assets.initialize_on_precompile = false
   end
 end
