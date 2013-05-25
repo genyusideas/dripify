ec0cd87f3472fed9b68371cb03f867762790635e
Jeff Mataya <jeff.mataya@gmail.com>
2013-05-25 11:07:21 -0700

    Finished tests and base method for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
 
   # GET /users/:id
   def show
-    render json: User.find( params[:id] )
+    begin
+      render json: User.find( params[:id] )
+    rescue
+      render json: { error: "User not found" }, status: 403
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,15 +4,46 @@ describe UsersController do
   render_views
 
   describe "when requesting an existing user" do
-    let!(:user) { FactoryGirl.create( :user ) } 
+    let!(:user) { FactoryGirl.create( :user ) }
+    before do 
+      get :show, id: user.id, format: :json
+      @body = JSON.parse(response.body)
+    end
 
     it "returns the correct user" do
-      get :show, id: user.id, format: :json
       response.should be_success
+    end
 
-      body = JSON.parse(response.body)
-      body.should include('email')
-      body['email'].should == user.email
+    it "should return the user in the correct format" do
+      @body.should include('email')
+      @body.should include('first_name')
+      @body.should include('last_name')
+    end
+
+    it "should be the correct user" do
+      @body['email'].should == user.email
+    end
+  end
+
+  describe "when requesting a user that doesn't exist" do
+    before do
+      id = User.last.id unless User.all.empty?
+      id = 1 if User.all.empty?
+      get :show, id: id, format: :json
+      @body = JSON.parse(response.body)
+    end
+
+    it "should not return any user" do
+      response.should_not be_success
+    end
+
+    it "should return the error message" do
+      @body.should include('error')
+      @body['error'].should == "User not found"
+    end
+
+    it "should return the error status" do
+      response.status.should == 403
     end
   end
 end
